4831fccaed9354c66eacc965e161e3a84e75dd87
veeay <#Bailey12>
2020-03-17 10:39:27 -0700

    Initial commit

---
+++ src/main/java/me/veeayeinin/minecraft/hostileanimals/HostileAnimals.java
@@ -0,0 +1,17 @@
+package me.veeayeinin.minecraft.hostileanimals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HostileAnimals extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
